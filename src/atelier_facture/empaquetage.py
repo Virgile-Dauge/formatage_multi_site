8121d0ed9b2f83b348c57b441ee551fc64316b02
Virgile <virgile.dauge@pm.me>
2024-10-02 13:39:54 +0200

    adaptation pour factures indiv

--- src/atelier_facture/empaquetage.py
+++ src/atelier_facture/empaquetage.py
@@ -24,13 +24,18 @@ def extract_metadata_and_update_df(pdf_files: list[Path], df: pd.DataFrame) -> p
 
     if 'BT-1' not in df_copy.columns:
         df_copy['BT-1'] = ''
+    if 'pdl' not in df_copy.columns and 'PRM' in df_copy.columns:
+        df_copy = df_copy.rename(columns={'PRM': 'pdl'})
     if 'pdl' not in df_copy.columns:
         df_copy['pdl'] = ''
     if 'group' not in df_copy.columns and 'facture' in df_copy.columns:
         df_copy = df_copy.rename(columns={'facture': 'group'})
     
-    df_copy['group'] = df_copy['group'].astype(str).str.strip()
-
+    if 'group' not in df_copy.columns:
+        df_copy['group'] = ''
+    df_copy['group'] = df_copy['group'].fillna('').astype(str).str.strip()
+    df_copy['pdl'] = df_copy['pdl'].fillna('').astype(str).str.strip()
+    
     for pdf_file in pdf_files:
         reader = PdfReader(pdf_file)
         metadata = reader.metadata
@@ -38,16 +43,18 @@ def extract_metadata_and_update_df(pdf_files: list[Path], df: pd.DataFrame) -> p
         group_name = metadata.get('/GroupName', '')
         group_name = str(group_name).strip()
         invoice_id = metadata.get('/InvoiceID', '')
-        pdl = metadata.get('/Pdl', '')
+        pdl = str(metadata.get('/Pdl', '')).strip()
         # Cherche d'abord une correspondance dans la colonne 'group'
-        mask = df_copy['group'].str.lower() == group_name.lower()
+        mask = df_copy['group'].astype(str).str.strip() == str(group_name).strip()
 
-        mask = mask if mask.any() else df_copy['pdl'] == pdl
-
+        # Check if any True values in group_mask
+        if not mask.any():
+            mask = df_copy['pdl'].fillna('').astype(str).str.strip() == pdl
+        
+        
         # First, convert the entire 'BT-1' column to string type
         df_copy['BT-1'] = df_copy['BT-1'].astype(str).apply(lambda x: x.rstrip('.0'))
         df_copy.loc[mask, 'BT-1'] = str(int(invoice_id))
         df_copy.loc[mask, 'pdf'] = pdf_file
-        #print(group_name, pdl, mask.any())
 
     return df_copy
